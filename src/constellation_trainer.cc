6062559af54da0a6489172e31a6f01f51b4f6cb7
DeEMO <yzzxrx@foxmail.com>
2024-07-04 15:15:28 +0800

    :construction: pushpull impl

--- src/constellation_trainer.cc
+++ src/constellation_trainer.cc
@@ -21,20 +21,22 @@ void ConstelTrainer::PushPull(std::vector<int>& keys,
                               std::vector<CArray>& vals_push,
                               std::vector<CArray*>& vals_pull) {
   std::vector<int> ts_arr;
-  auto res_map = std::make_shared<std::unordered_map<int, int>>();
-  std::vector<UpdateBuf> vals;
+  int size = keys.size();
+  std::unordered_map<int, int> res_map;
+  std::vector<UpdateBuf> vals(size);
   // TODO: need consider merge the key-value if there more than one gpus
-  for (size_t i = 0; i < keys.size(); i++) {
+  for (size_t i = 0; i < size; i++) {
     int key = keys[i];
     // submit the key-value, and get the timestamp which the corresponding merged value is pushed to
     // the father node
-    UpdateBuf updt;
+    auto& updt = vals[i];
     updt.merged = vals_push[i];
-    vals.push_back(updt);
   }
   engine_->PushAndWait(std::move(keys), std::move(vals), res_map);
-  for (auto it : *res_map) {
+  for (auto& it : res_map) {
     trainer_->Wait(it.second);
+    //1. 对于所有的meta 还有没记录, 是在recive的时候记录的
+    //2. 用记录的meta去回复子节点，带上pull回来的数据
   }
 }
 
@@ -59,11 +61,11 @@ void ConstelTrainer::ProcessPushData(const int key,
     update_buf->num++;
   } else {
     // TODO: Use Template to replace this
-    auto dst = reinterpret_cast<float*>(update_buf->merged.data);
-    auto src = reinterpret_cast<float*>(update_buf->merged.data);
+    auto dst = reinterpret_cast<float*>(update_buf->merged.data());
+    auto src = reinterpret_cast<float*>(update_buf->merged.data());
     // TODO: Use omp
-    CHECK_EQ(update_buf->merged.size, data.merged.size);
-    for (size_t i = 0; i < update_buf->merged.size / sizeof(float); i++) {
+    CHECK_EQ(update_buf->merged.size(), data.merged.size());
+    for (size_t i = 0; i < update_buf->merged.size() / sizeof(float); i++) {
       dst[i] += src[i];
     }
   }
@@ -74,10 +76,10 @@ void ConstelTrainer::ProcessPushData(const int key,
   if (update_buf->num == ps::Postoffice::Get()->GetMyChildren().size() + 1) {
     // send to father
     // TODO: int dtype = what...
-    ps::SArray<char> vals(update_buf->merged.data, update_buf->merged.size, false);
+    ps::SArray<char> vals(update_buf->merged.data(), update_buf->merged.size(), false);
     auto key_t = static_cast<ps::Key>(key);
     ps::SArray<ps::Key> keys(&key_t, 1, false);
-    auto len_t = static_cast<int>(update_buf->merged.size);
+    auto len_t = static_cast<int>(update_buf->merged.size());
     ps::SArray<int> lens(&len_t, 1, false);
     int cmd = GetCommandType(RequestType::kDefaultPushPull, update_buf->merged.dtype);
     // TODO: add pull calback
@@ -128,12 +130,15 @@ void ConstelTrainer::DataHandle(const ps::KVMeta& req_meta,
                                 const ps::KVPairs<char>& req_data,
                                 ps::KVTrainer<char>* trainer) {
   DataHandleType type = DepairDataHandleType(req_meta.cmd);
+  UpdateBuf updt;
   switch (type.requestType) {
     case RequestType::kDefaultPushPull:
-      UpdateBuf updt;
       updt.request_meta.push_back(req_meta);
-      updt.merged.CopyFrom(req_data.vals[0]);
-      //TODO:
+      updt.merged = CArray(req_data.lens[0]);
+      // 先数据拷贝一份，有优化空间
+      updt.merged.CopyFrom((void*)req_data.vals.data(), req_data.lens[0]);
+      // 收的逻辑还没有写
+      // 存meta，meta的管理（还没想好）
       break;
     default:
       LOG(FATAL) << "Unkown request type";
