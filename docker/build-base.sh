83ecec50a7444fcebb10957cc5c3a2d5c0621830
DeEMO <yzzxrx@foxmail.com>
2025-01-24 13:16:36 +0000

    Enhance Docker setup: update base images, add build scripts, and improve docker-compose configuration

--- docker/build-base.sh
+++ docker/build-base.sh
@@ -2,4 +2,13 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build $DIR -f $DIR/dockerfile-base  -t constellation-base
+# extract the base image name from the Dockerfile
+PYTORCH_BASE_IMAGE=$(grep FROM $DIR/dockerfile-base | awk '{print $2}')
+# extract the base image tag
+PYTORCH_BASE_TAG=$(echo $PYTORCH_BASE_IMAGE | awk -F: '{print $2}')
+# combine pytorch- and the tag
+BASE_IMAGE_TAG=pytorch-$PYTORCH_BASE_TAG
+BASE_IMAGE=constellation-base:$BASE_IMAGE_TAG
+echo "$BASE_IMAGE"
+
+docker build $DIR -f $DIR/dockerfile-base  -t $BASE_IMAGE
